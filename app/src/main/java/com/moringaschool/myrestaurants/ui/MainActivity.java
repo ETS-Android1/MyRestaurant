a4d5f0567cbe72168d053fc47540b5718fb360ad
mabunde <solomonsitati39@gmail.com>
2021-12-13 12:59:35 +0300

    Refactor main activity class

--- app/src/main/java/com/moringaschool/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/ui/MainActivity.java
@@ -4,65 +4,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.moringaschool.myrestaurants.Constants;
 import com.moringaschool.myrestaurants.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-//    private SharedPreferences mSharedPreferences;
-//    private SharedPreferences.Editor mEditor;
-    private DatabaseReference mSearchedLocationReference;
-    private ValueEventListener mSearchedLocationReferenceListener;
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
-    @BindView(R.id.locationEditText) EditText mLocationEditText;
     @BindView(R.id.appNameTextView) TextView mAppNameTextView;
     @BindView(R.id.savedRestaurantsButton) Button mSavedRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        mSearchedLocationReference= FirebaseDatabase
-                .getInstance()
-                .getReference()
-                .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);
-
-        mSearchedLocationReferenceListener= mSearchedLocationReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for(DataSnapshot locationSnapshot: dataSnapshot.getChildren()){
-                    String location = locationSnapshot.getValue().toString();
-
-                }
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-
-            }
-        });
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-//        mEditor = mSharedPreferences.edit();
 
         mFindRestaurantsButton.setOnClickListener(this);
         mSavedRestaurantsButton.setOnClickListener(this);
@@ -70,30 +32,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if (v == mFindRestaurantsButton) {
-            String location = mLocationEditText.getText().toString();
-            saveLocationToFirebase(location);
-//            if(!(location).equals("")) {
-//                addToSharedPreferences(location);
-//            }
+
+        if(v == mFindRestaurantsButton) {
             Intent intent = new Intent(MainActivity.this, RestaurantListActivity.class);
-            intent.putExtra("location",location);
             startActivity(intent);
         }
+
         if (v == mSavedRestaurantsButton) {
             Intent intent = new Intent(MainActivity.this, SavedRestaurantListActivity.class);
             startActivity(intent);
         }
-    }
-    public void saveLocationToFirebase(String location){
-        mSearchedLocationReference.push().setValue(location);
-    }
-//    private void addToSharedPreferences(String location) {
-//        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
-//    }
-@Override
-protected void onDestroy() {
-    super.onDestroy();
-    mSearchedLocationReference.removeEventListener(mSearchedLocationReferenceListener);
     }
 }
